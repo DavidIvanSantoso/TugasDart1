7b1f777d119f54b3c83db0d1e176732d9f988420
DavidIvanSantoso <davidivan6900@gmail.com>
2022-03-07 22:04:57 +0700

    no1

--- bin/dart_tugas_1.dart
+++ bin/dart_tugas_1.dart
@@ -5,8 +5,54 @@ void main(List<String> arguments) {
   String? N=stdin.readLineSync();
   int NInt=int.parse(N!);
 
+  //link github
+  //https://github.com/DavidIvanSantoso/TugasDart1
 
-  //no 2
+  //no 1
+  int cnt=0;
+  for(int i=0;i<=(NInt/2).ceil();i++){
+    for(int j=i;j<(NInt/2).ceil();j++){
+      if(i!=0){
+        stdout.write("*");
+      }
+    }
+    for(int z=0;z<i;z++){
+      stdout.write(cnt);
+      if(z+1!=i){
+        stdout.write(" ");
+      }
+    }
+    for(int j=i;j<(NInt/2).ceil();j++){
+      if(i!=0){
+         stdout.write("*");
+      }
+    }
+    print("");
+    cnt++;
+  }
+  for(int i=(NInt/2).floor();i>0;i--){
+    for(int j=(NInt/2).floor();j>=i;j--){
+      if(i!=0){
+        stdout.write("*");
+      }
+    }
+    for(int z=0;z<i;z++){
+      stdout.write(cnt);
+      if(z+1!=i){
+        stdout.write(" ");
+      }
+    }
+    for(int j=(NInt/2).floor();j>=i;j--){
+      if(i!=0){
+         stdout.write("*");
+      }
+    }
+    print("");
+    cnt++;
+  }
+  print ("");
+
+  //no 2 
   int start=1;
   int end=NInt;
   for(int i=0;i<NInt;i++)
