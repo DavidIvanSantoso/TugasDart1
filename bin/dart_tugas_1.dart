f33951d758fc92f12278e2b09ecae3f8ac253256
DavidIvanSantoso <davidivan6900@gmail.com>
2022-03-07 21:23:37 +0700

    No2

--- bin/dart_tugas_1.dart
+++ bin/dart_tugas_1.dart
@@ -1,3 +1,42 @@
+import 'dart:io';
+import 'dart:convert';
 void main(List<String> arguments) {
-  print('Hello world!');
+  print("Input N");
+  String? N=stdin.readLineSync();
+  int NInt=int.parse(N!);
+
+
+  //no 2
+  int start=1;
+  int end=NInt;
+  for(int i=0;i<NInt;i++)
+  {
+    for(int j=NInt-1;j>=0;j--)
+    {
+      if(start>NInt){
+        start=1;
+      }
+      if(j<i){
+        stdout.write(" ");
+      }
+      else{
+        stdout.write(start);
+        start+=1;
+      }  
+    }
+    for(int z=0;z<NInt;z++){
+      if(end==0){
+        end=NInt;
+      }
+       if(z<i){
+        stdout.write(" ");
+      }
+      else{
+        stdout.write(end);
+        end--;
+      }
+    }
+    print(""); 
+  } 
 }
+
